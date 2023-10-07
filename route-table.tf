f41f556eaf3dd9ce45b1c259eacfc897585b932b
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-08 00:50:48 +0530

    Auto push

--- route-table.tf
+++ route-table.tf
@@ -13,6 +13,17 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+# Associates the route-table to the public-subnet
+
+resource "aws_route_table_association" "public_subnet_rt_association" {
+  count           = length(aws_subnet.public_subnet.*.id)  // * includes everythin subnets ids of all the subnets created by aws subnet.public subnet
+ 
+  subnet_id       = element(aws_subnet.public_subnet.*.id , count.index)
+  route_table_id  = aws_route_table.public_rt.id
+ }
+
+
+
 # creates Private Route table
 # resource "aws_route_table" "private_rt" {
 #   vpc_id         =  aws_vpc.main.id
