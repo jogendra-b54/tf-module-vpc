372e1494d7932d27340f9893224aaac4a3176936
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-08 00:56:30 +0530

    Auto push

--- route-table.tf
+++ route-table.tf
@@ -1,4 +1,4 @@
-#creates public Route table
+# Creates public Route table
 
 resource "aws_route_table" "public_rt" {
   vpc_id         =  aws_vpc.main.id
@@ -24,16 +24,25 @@ resource "aws_route_table_association" "public_subnet_rt_association" {
 
 
 
-# creates Private Route table
-# resource "aws_route_table" "private_rt" {
-#   vpc_id         =  aws_vpc.main.id
+ # Creates Private Route table
+resource "aws_route_table" "private_rt" {
+  vpc_id         =  aws_vpc.main.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = aws_internet_gateway.igw.id
+  # }
 
-#   tags = {
-#         Name = "roboshop-${var.ENV}-public-rt"
-#   }
-# }
+  tags = {
+        Name = "roboshop-${var.ENV}-private-rt"
+  }
+}
+
+# Associates the route-table to the private-subnet
+
+resource "aws_route_table_association" "private_subnet_rt_association" {
+  count           = length(aws_subnet.private_subnet.*.id)  // * includes everythin subnets ids of all the subnets created by aws subnet.public subnet
+ 
+  subnet_id       = element(aws_subnet.private_subnet.*.id , count.index)
+  route_table_id  = aws_route_table.private_rt.id
+ }
