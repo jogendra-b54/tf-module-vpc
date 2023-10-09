27690554feb6e35bec577c0103e214211589be9e
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-09 12:01:44 +0530

    Auto push

---
+++ ngw.tf
@@ -0,0 +1,23 @@
+# Provision EIP needed for NAT Gateways
+
+resource "aws_eip" "ngw_eip" {
+    vpc         =   true
+
+    tags = {
+        Name = "roboshop-${var.ENV}-ngw-eip"
+    }
+}
+
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.ngw_eip.id
+  subnet_id     = aws_subnet.public_subnet.*.id[0]
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway and the EIP the ngw 
+  depends_on = [aws_eip.ngw_eip,aws_internet_gateway.igw]
+}
--- route-table.tf
+++ route-table.tf
@@ -28,10 +28,10 @@ resource "aws_route_table_association" "public_subnet_rt_association" {
 resource "aws_route_table" "private_rt" {
   vpc_id         =  aws_vpc.main.id
 
-  # route {
-  #   cidr_block = "0.0.0.0/0"
-  #   gateway_id = aws_internet_gateway.igw.id
-  # }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
   tags = {
         Name = "roboshop-${var.ENV}-private-rt"
