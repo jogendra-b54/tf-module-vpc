bf92323ab0d1d6d1d0a8758a138a73d2d0bfceb1
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-09 15:03:36 +0530

    Auto push

---
+++ peer.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  vpc_id        = aws_vpc.main.id
+  peer_vpc_id   = "vpc-0c191255a724f193a"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  auto_accept             = true
+
+  tags = {
+    Side = "Robot-${var.ENV}-default-vpc-peering"
+  }
+}
