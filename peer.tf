e66f3b0f37c37e526a9680be4e6b236f26892a6a
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-09 16:11:52 +0530

    Auto push

--- peer.tf
+++ peer.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id   = aws_vpc.main.id
   vpc_id        = aws_vpc.main.id
-  vpc_peering_connection_id = var.DEFAULT_VPC_ID
+  //vpc_peering_connection_id = var.DEFAULT_VPC_ID
   auto_accept   = true
 
   tags = {
